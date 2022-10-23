3ab7dcd20c8ec9761821377443876b832515cf6c
Basia Białostocka <basiabialostocka@gmail.com>
2022-10-23 17:52:43 +0200

    Initial project structure

---
+++ project/NextflowRedemption/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+
+def index(request):
+    context = { 'klucz': "test" }
+    return render(request, 'NextflowRedemption/index.html', context)
+    #return HttpResponse("Hello, world.")
