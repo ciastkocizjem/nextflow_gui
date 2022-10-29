c003af5c626fa824e6112dfa5b267fc0ec3fdad6
micfal <micfal@wp.pl>
2022-10-29 13:14:45 +0200

    Dodane dwie strony i wstępny html na podstawie makiety

--- project/NextflowRedemption/views.py
+++ project/NextflowRedemption/views.py
@@ -6,3 +6,7 @@ def index(request):
     context = { 'klucz': "test" }
     return render(request, 'NextflowRedemption/index.html', context)
     #return HttpResponse("Hello, world.")
+
+def config(request):
+    context = { 'klucz': "test" }
+    return render(request, 'Config/index.html', context)
--- project/project/urls.py
+++ project/project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('nextflowRedemption', include('NextflowRedemption.urls')),
+    path('main/', include('NextflowRedemption.urls')),
 ]
+
