9813e244950003640dd69f9de0050a507b68ba4c
unysa <unysa16@gmail.com>
2022-11-12 17:50:24 +0100

    Added add and edit for pipelines

--- project/NextflowRedemption/urls.py
+++ project/NextflowRedemption/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('config',views.config, name='config'),
-    path('runPipe', views.PipeTest,name = 'PipeTest'),
-    path('getPipeProg', views.PipeProgress,name = 'PipeProgress')
+    path('config', views.config, name='config'),
+    path('config/<int:id>', views.configEdit, name='configEdit'),
+    path('runPipe', views.PipeTest, name='PipeTest'),
+    path('tableData', views.TableData, name='TableData'),
+    path('getPipeProg', views.PipeProgress,name='PipeProgress')
 ]
--- project/NextflowRedemption/views.py
+++ project/NextflowRedemption/views.py
@@ -24,9 +24,71 @@ def index(request):
     context = { 'presets': presets, 'pipes': pipes }
     return render(request, 'NextflowRedemption/index.html', context)
 
+
 def config(request):
-    context = { 'klucz': "test" }
+    if(request.method == 'POST'):
+        presets = load("template")
+        data = request.POST
+        name = data.get("name")
+        pipeline = request.FILES.get("pipeline")
+        print(request.FILES)
+        config = request.FILES.get("config")
+        presets.append({
+            "py/object": "NextflowRedemption.models.Template",
+		    "id": len(presets),
+		    "name": name,
+		    "pipeline": {
+			    "py/object": "nextflow.pipeline.Pipeline",
+			    "path": pipeline,
+			    "config": config
+		    }
+        })
+        pipes = load("pipeline")
+        save("template", presets)
+        save("pipeline", pipes)
+        context = { 'presets': presets, 'pipes': pipes }
+        return render(request, 'NextflowRedemption/index.html', context)
+    presets = load("template")
+    pipes = load("pipeline")
+    context = { 'presets': presets, 'pipes': pipes}
     return render(request, 'Config/index.html', context)
+
+def configEdit(request, id=None):
+    if(request.method == 'POST'):
+        pipes = load("pipeline")
+        presets = load("template")
+        data = request.POST
+        name = presets[id].name + "_" + data.get("name")
+        pipeline = data.get("pipeline")
+        config = data.get("config")
+        pipes.append({
+            "py/object": "NextflowRedemption.models.Pipeline",
+		    "id": len(pipes),
+		    "name": name,
+		    "pipeline": {
+			    "py/object": "nextflow.pipeline.Pipeline",
+			    "path": pipeline,
+			    "config": config,
+                "parameters": [
+                    {"name": "aaaa", "value": "bbbb"},
+                    {"name": "aaaa", "value": "bbbb"}
+                ]
+		    }
+        })
+        save("pipeline", pipes)
+        presets = load("template")
+        pipes = load("pipeline")
+        context = { 'presets': presets, 'pipes': pipes }
+        return render(request, 'NextflowRedemption/index.html', context)
+    presets = load("template")
+    pipes = load("pipeline")
+    context = { 'preset': presets[id]}
+    return render(request, 'Config/edit.html', context)
+
+def TableData(request):
+    val1 = request.POST['table']
+    return JsonResponse({"_":""},status=200)
+
 
 def PipeTest(request):
     pipeline1 = nextflow.Pipeline("pipelines/my-pipeline.nf")
